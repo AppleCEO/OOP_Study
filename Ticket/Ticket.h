44ae41cf9f1e9793abe8a450679294ac499ac9a3
Ben <ppth0608@naver.com>
2019-07-28 23:06:10 +0900

    Ticket 프레임워크 생성

---
+++ Ticket/Ticket.h
@@ -0,0 +1,19 @@
+//
+//  Ticket.h
+//  Ticket
+//
+//  Created by Ben on 2019/07/28.
+//  Copyright © 2019 Ben. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ticket.
+FOUNDATION_EXPORT double TicketVersionNumber;
+
+//! Project version string for Ticket.
+FOUNDATION_EXPORT const unsigned char TicketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ticket/PublicHeader.h>
+
+
